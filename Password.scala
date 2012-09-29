6f88ffdf40a9bdec86c1b64df076f787accd8786
Sean Rhea <sean.c.rhea@gmail.com>
2012-09-29 09:57:46 -0700

    rewrite as an object
    ...in preparation for adding more methods.  Also rename constants to
    match Scala style.

--- Password.scala
+++ Password.scala
@@ -1,29 +1,33 @@
-val LOWERS = "abcdefghijklmnopqrstuvwxyz"
-val UPPERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-val NUMS = "0123456789"
-val SYMS = """`-=[]\;',./~!@#$%^&*()_+{}|:"<>?"""
-val ALLCHARS = LOWERS + UPPERS + NUMS + SYMS
+import java.security.SecureRandom
 
-var len = 8
-if (args.length > 0) {
-  len = Integer.parseInt(args(0))
-  if (args.length > 1 || len < 4) {
-    println("usage: password <length>")
-    sys.exit(1)
+object Password {
+  def generate(length: Int): String = {
+    val Lowers = "abcdefghijklmnopqrstuvwxyz"
+    val Uppers = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    val Digits = "0123456789"
+    val Symbols = """`-=[]\;',./~!@#$%^&*()_+{}|:"<>?"""
+    val Groups = Array(Lowers, Uppers, Digits, Symbols)
+    val All = Groups.mkString("")
+
+    val rand = new SecureRandom
+    (1 to 100).foreach { attempt =>
+      val result = (1 to length).map { i => All(rand.nextInt(All.length)) }.mkString("")
+      val missing = Groups.filter { group => result.filter { c => group.contains(c) }.isEmpty }
+      if (missing.isEmpty)
+        return result
+    }
+    throw new Exception("failed to find a good password")
+  }
+
+  def main(args: Array[String]) {
+    var length = 8
+    if (args.length > 0) {
+      length = Integer.parseInt(args(0))
+      if (args.length > 1 || length < 4) {
+        println("usage: password <length>")
+        sys.exit(1)
+      }
+    }
+    println(generate(length))
   }
 }
-
-val rand = new java.security.SecureRandom
-(1 to 10).foreach { attempt =>
-  val pw = (1 to len).map { i => ALLCHARS(rand.nextInt(ALLCHARS.length)) }.mkString("")
-  val missing = Array(LOWERS, UPPERS, NUMS, SYMS).filter { group =>
-    pw.filter { char => group.contains(char) }.isEmpty
-  }
-  if (missing.isEmpty) {
-    println(pw)
-    sys.exit(0)
-  }
-}
-
-println("failed to find a good password")
-sys.exit(1)
