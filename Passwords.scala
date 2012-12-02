2c4ff4fe3978c6bb2d8169ac3f056dcc2444ffb6
Sean Rhea <sean.c.rhea@gmail.com>
2012-12-02 11:11:01 -0800

    change to compiled code, add bin script
    Using scala as a script is just too slow. Compile and run with java
    instead. Use 'sbt compile' to build, then bin/passwords to run. This
    diff is actually much smaller than it looks, but I renamed a file.

--- Passwords.scala
+++ Passwords.scala
@@ -1,8 +1,3 @@
-#!/bin/sh
-# vim: set filetype=scala:sw=2:tw=99:expandtab:
-exec scala "$0" "$@"
-!#
-
 // Copyright (c) 2012 Sean C. Rhea <sean.c.rhea@gmail.com>
 // See LICENSE file for details.
 
@@ -155,144 +150,148 @@ class Vault(file: File, val password: Array[Char]) {
   }
 }
 
-def generate(length: Int): String = {
-  val Lowers = "abcdefghijklmnopqrstuvwxyz"
-  val Uppers = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  val Digits = "0123456789"
-  val Symbols = """`-=[]\;',./~!@#$%^&*()_+{}|:"<>?"""
-  val Groups = Array(Lowers, Uppers, Digits, Symbols)
-  val All = Groups.mkString("")
-  val rand = new SecureRandom
-  for (attempt <- (1 to 100)) {
-    val result = (1 to length).map { i => All(rand.nextInt(All.length)) }.mkString("")
-    val missing = Groups.filter { group => result.filter { c => group.contains(c) }.isEmpty }
-    if (missing.isEmpty)
-      return result
-  }
-  println("Failed to find a good password.")
-  sys.exit(1)
-}
-
-def usage() {
-  println("""|Usage:
-             |  passwords <command> [<args>]
-             |Commands:
-             |  generate [length]
-             |  add <url> <username>
-             |  remove <url> <username>
-             |  list
-             |  search <search string>
-             |  merge <other file>""".stripMargin)
-  sys.exit(1)
-}
-
-def confirm(msg: String) {
-  val yesno = System.console.readLine(msg)
-  if (!Array("y", "yes").contains(yesno.toLowerCase))
+object Passwords {
+  def generate(length: Int): String = {
+    val Lowers = "abcdefghijklmnopqrstuvwxyz"
+    val Uppers = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    val Digits = "0123456789"
+    val Symbols = """`-=[]\;',./~!@#$%^&*()_+{}|:"<>?"""
+    val Groups = Array(Lowers, Uppers, Digits, Symbols)
+    val All = Groups.mkString("")
+    val rand = new SecureRandom
+    for (attempt <- (1 to 100)) {
+      val result = (1 to length).map { i => All(rand.nextInt(All.length)) }.mkString("")
+      val missing = Groups.filter { group => result.filter { c => group.contains(c) }.isEmpty }
+      if (missing.isEmpty)
+        return result
+    }
+    println("Failed to find a good password.")
     sys.exit(1)
-}
-
-def init(file: File, exitIfMissing: Boolean): Pair[Vault, Map[Login, Password]] = {
-  val bak = new File(file.getAbsolutePath + ".bak")
-  if (!file.exists && bak.exists) {
-    // Recover from previous crash.
-    bak.renameTo(file)
   }
-  if (file.exists) {
-    val masterPassword = System.console.readPassword("Master password: ")
-    val vault = new Vault(file, masterPassword)
-    val db = try { vault.read } catch {
-      case e: Exception => println("Password incorrect."); sys.exit(1)
-    }
-    (vault, db)
-  } else {
-    println("No existing passwords database.")
-    if (exitIfMissing)
-      sys.exit(1)
-    val first = System.console.readPassword("Enter a master password: ")
-    val second = System.console.readPassword("Confirm master password: ")
-    if (!first.sameElements(second)) {
-      println("Passwords do not match.")
-      sys.exit(1)
-    }
-    val vault = new Vault(file, first)
-    val db = Map[Login, Password]()
-    vault.write(db)
-    (vault, db)
+
+  def usage() {
+    println("""|Usage:
+               |  passwords <command> [<args>]
+               |Commands:
+               |  generate [length]
+               |  add <url> <username>
+               |  remove <url> <username>
+               |  list
+               |  search <search string>
+               |  merge <other file>""".stripMargin)
+    sys.exit(1)
   }
-}
 
-if (args.length == 0)
-  usage()
-
-val file = new File(System.getProperty("user.home"), ".passwords")
-
-args.head match {
-  case "generate" =>
-    val length = if (args.length > 1) Integer.parseInt(args(1)) else 8
-    if (length < 4) {
-      println("Length must be at least 4.")
+  def confirm(msg: String) {
+    val yesno = System.console.readLine(msg)
+    if (!Array("y", "yes").contains(yesno.toLowerCase))
       sys.exit(1)
+  }
+
+  def init(file: File, exitIfMissing: Boolean): Pair[Vault, Map[Login, Password]] = {
+    val bak = new File(file.getAbsolutePath + ".bak")
+    if (!file.exists && bak.exists) {
+      // Recover from previous crash.
+      bak.renameTo(file)
     }
-    println(generate(length))
-  case "list" =>
-    val (vault, db) = init(file, true)
-    for ((login, password) <- SortedMap(db.toSeq: _*)) {
-      println(login.url + " " + login.user)
+    if (file.exists) {
+      val masterPassword = System.console.readPassword("Master password: ")
+      val vault = new Vault(file, masterPassword)
+      val db = try { vault.read } catch {
+        case e: Exception => println("Password incorrect."); sys.exit(1)
+      }
+      (vault, db)
+    } else {
+      println("No existing passwords database.")
+      if (exitIfMissing)
+        sys.exit(1)
+      val first = System.console.readPassword("Enter a master password: ")
+      val second = System.console.readPassword("Confirm master password: ")
+      if (!first.sameElements(second)) {
+        println("Passwords do not match.")
+        sys.exit(1)
+      }
+      val vault = new Vault(file, first)
+      val db = Map[Login, Password]()
+      vault.write(db)
+      (vault, db)
     }
-  case "search" =>
-    if (args.length < 2)
+  }
+
+  def main(args: Array[String]) {
+    if (args.length == 0)
       usage()
-    val search = args(1)
-    val (vault, db) = init(file, true)
-    val matches = db.filter { case (login, password) => login.url.contains(search) }
-    if (matches.isEmpty)
-      println("No passwords for urls matching \"%s\".".format(search))
-    for ((login, password) <- matches) {
-      println(login.url + " " + login.user + " " + password.pw)
+
+    val file = new File(System.getProperty("user.home"), ".passwords")
+
+    args.head match {
+      case "generate" =>
+        val length = if (args.length > 1) Integer.parseInt(args(1)) else 8
+        if (length < 4) {
+          println("Length must be at least 4.")
+          sys.exit(1)
+        }
+        println(generate(length))
+      case "list" =>
+        val (vault, db) = init(file, true)
+        for ((login, password) <- SortedMap(db.toSeq: _*)) {
+          println(login.url + " " + login.user)
+        }
+      case "search" =>
+        if (args.length < 2)
+          usage()
+        val search = args(1)
+        val (vault, db) = init(file, true)
+        val matches = db.filter { case (login, password) => login.url.contains(search) }
+        if (matches.isEmpty)
+          println("No passwords for urls matching \"%s\".".format(search))
+        for ((login, password) <- matches) {
+          println(login.url + " " + login.user + " " + password.pw)
+        }
+      case "add" =>
+        if (args.length < 3)
+          usage()
+        val login = Login(args(1), args(2))
+        val (vault, db) = init(file, false)
+        if (db.contains(login))
+          confirm("Overwrite existing entry? (Y/N): ")
+        val password = new String(System.console.readPassword("Password: "))
+        val again = new String(System.console.readPassword("Confirm password: "))
+        if (password != again) {
+          println("Passwords do not match.")
+          sys.exit(1)
+        }
+        vault.write(db + (login -> Password(password, System.currentTimeMillis)))
+      case "remove" =>
+        if (args.length < 3)
+          usage()
+        val login = Login(args(1), args(2))
+        val (vault, db) = init(file, true)
+        if (!db.contains(login)) {
+          println("No matching url and username. Try \"passwords search " + login.url + "\".")
+          sys.exit(0)
+        }
+        confirm("Really remove this entry? (Y/N): ")
+        vault.write(db - login)
+      case "merge" =>
+        if (args.length < 2)
+          usage()
+        val otherFile = new File(args(1))
+        val (vault, db) = init(file, true)
+        val otherVault = new Vault(otherFile, vault.password)
+        val otherDb = try { otherVault.read } catch {
+          case e: Exception => println("Master password in " + args(1) + " differs."); sys.exit(1)
+        }
+        val onlyInDb = db.filterKeys { k => !otherDb.contains(k) }
+        val onlyInOtherDb = otherDb.filterKeys { k => !db.contains(k) }
+        val inBoth = for {
+          (login, password) <- db
+          otherPassword <- otherDb.get(login)
+        } yield {
+          (login, (if (password.mtime > otherPassword.mtime) password else otherPassword))
+        }
+        vault.write(onlyInDb ++ onlyInOtherDb ++ inBoth)
+      case _ => usage()
     }
-  case "add" =>
-    if (args.length < 3)
-      usage()
-    val login = Login(args(1), args(2))
-    val (vault, db) = init(file, false)
-    if (db.contains(login))
-      confirm("Overwrite existing entry? (Y/N): ")
-    val password = new String(System.console.readPassword("Password: "))
-    val again = new String(System.console.readPassword("Confirm password: "))
-    if (password != again) {
-      println("Passwords do not match.")
-      sys.exit(1)
-    }
-    vault.write(db + (login -> Password(password, System.currentTimeMillis)))
-  case "remove" =>
-    if (args.length < 3)
-      usage()
-    val login = Login(args(1), args(2))
-    val (vault, db) = init(file, true)
-    if (!db.contains(login)) {
-      println("No matching url and username. Try \"passwords search " + login.url + "\".")
-      sys.exit(0)
-    }
-    confirm("Really remove this entry? (Y/N): ")
-    vault.write(db - login)
-  case "merge" =>
-    if (args.length < 2)
-      usage()
-    val otherFile = new File(args(1))
-    val (vault, db) = init(file, true)
-    val otherVault = new Vault(otherFile, vault.password)
-    val otherDb = try { otherVault.read } catch {
-      case e: Exception => println("Master password in " + args(1) + " differs."); sys.exit(1)
-    }
-    val onlyInDb = db.filterKeys { k => !otherDb.contains(k) }
-    val onlyInOtherDb = otherDb.filterKeys { k => !db.contains(k) }
-    val inBoth = for {
-      (login, password) <- db
-      otherPassword <- otherDb.get(login)
-    } yield {
-      (login, (if (password.mtime > otherPassword.mtime) password else otherPassword))
-    }
-    vault.write(onlyInDb ++ onlyInOtherDb ++ inBoth)
-  case _ => usage()
+  }
 }
---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "Passwords"
+
+organization := "srhea.net"
+
+scalaVersion := "2.9.1"
